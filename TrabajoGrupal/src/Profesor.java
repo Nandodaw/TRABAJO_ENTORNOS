6cb5bef1df70fb4b2bcb3a39d5c83a2420c8ff6f
tates1993 <tatesgomez@gmail.com>
2020-02-05 19:21:29 +0100

    Realizada mi parte de la tarea #11

--- TrabajoGrupal/src/Profesor.java
+++ TrabajoGrupal/src/Profesor.java
@@ -1,5 +1,13 @@
 
 public class Profesor {
-
+	private String sNombre;
+	private String sApellidos;
+	private String sDni;
+	private int iEdad;
+	private int iSueldo;
+	private int iAntiguedad;
+	private String sUniversidad;
+	private int iNumeroAlumnos;
+	private boolean bFuncionario;
 	
 }
