43d1ecc37d704646ee0caac99920b273e4070acf
Luismopls <luismivm28@gmail.com>
2020-02-05 20:06:35 +0100

    Finalizada la primera parte de la tarea #13.

---
+++ TrabajoGrupal/src/ITrabajador.java
@@ -0,0 +1,13 @@
+
+public interface ITrabajador {
+
+	public int getiSueldo();
+	
+	public void setiSueldo(int iSueldo);
+	
+	public int getiAntiguedad();
+	
+	public void setiAntiguedad(int iAntiguedad);
+	
+	
+}
