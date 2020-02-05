5b4a92121c1f751cf4001aaa69e44b58a986440d
Luismopls <luismivm28@gmail.com>
2020-02-05 21:37:41 +0100

    Se implementan la mitad de valores a mostrar en el main, falta la segunda mitad de la tarea #19

--- TrabajoGrupal/src/Principal.java
+++ TrabajoGrupal/src/Principal.java
@@ -2,10 +2,21 @@
 public class Principal {
 
 	public static void main(String[] args) {
-		IProfesor p1 = (IProfesor) new Profesor("Carlos", "García Ramos", "28.987.654-Z", 31);
-		IPersona p2 = (IPersona) new Profesor("Manuel", "Vázquez López", "16.085.624-A", 29);
-		ITrabajador p3 = (ITrabajador) new Profesor("Rosa", "Martos Rodríguez", "75.731.756-R", 27);
+		IProfesor p1 = (IProfesor) new Profesor("Carlos", "GarcÃ­a Ramos", "28.987.654-Z", 31);
+		IPersona p2 = (IPersona) new Profesor("Manuel", "VÃ¡zquez LÃ³pez", "16.085.624-A", 29);
+		ITrabajador p3 = (ITrabajador) new Profesor("Rosa", "Martos RodrÃ­guez", "75.731.756-R", 27);
 
+		p1.setiSueldo(1500);
+		p1.setiAntiguedad(1);
+		p1.setsUniversidad("Universidad Pablo de Olavide");
+		p1.setiNumeroAlumnos(150);
+		p1.setbFuncionario(false);
+		((ITrabajador) p2).setiSueldo(1400);
+		((ITrabajador) p2).setiAntiguedad(0);
+		((IProfesor) p2).setsUniversidad("Universidad Internacional de Andalucï¿½a");
+		((IProfesor) p2).setiNumeroAlumnos(350);
+		((IProfesor) p2).setbFuncionario(false);
+		
 	}
 
 }
