85aa15f97a0f7498c8afa87342e1df6bc435c3d0
Luismopls <luismivm28@gmail.com>
2020-02-05 19:36:15 +0100

    Finalizada la generación de los getters/setters. closed#11

--- TrabajoGrupal/src/Profesor.java
+++ TrabajoGrupal/src/Profesor.java
@@ -1,5 +1,6 @@
 
 public class Profesor {
+
 	private String sNombre;
 	private String sApellidos;
 	private String sDni;
@@ -9,5 +10,77 @@ public class Profesor {
 	private String sUniversidad;
 	private int iNumeroAlumnos;
 	private boolean bFuncionario;
-	
+
+	public String getsNombre() {
+		return sNombre;
+	}
+
+	public void setsNombre(String sNombre) {
+		this.sNombre = sNombre;
+	}
+
+	public String getsApellidos() {
+		return sApellidos;
+	}
+
+	public void setsApellidos(String sApellidos) {
+		this.sApellidos = sApellidos;
+	}
+
+	public String getsDni() {
+		return sDni;
+	}
+
+	public void setsDni(String sDni) {
+		this.sDni = sDni;
+	}
+
+	public int getiEdad() {
+		return iEdad;
+	}
+
+	public void setiEdad(int iEdad) {
+		this.iEdad = iEdad;
+	}
+
+	public int getiSueldo() {
+		return iSueldo;
+	}
+
+	public void setiSueldo(int iSueldo) {
+		this.iSueldo = iSueldo;
+	}
+
+	public int getiAntiguedad() {
+		return iAntiguedad;
+	}
+
+	public void setiAntiguedad(int iAntiguedad) {
+		this.iAntiguedad = iAntiguedad;
+	}
+
+	public String getsUniversidad() {
+		return sUniversidad;
+	}
+
+	public void setsUniversidad(String sUniversidad) {
+		this.sUniversidad = sUniversidad;
+	}
+
+	public int getiNumeroAlumnos() {
+		return iNumeroAlumnos;
+	}
+
+	public void setiNumeroAlumnos(int iNumeroAlumnos) {
+		this.iNumeroAlumnos = iNumeroAlumnos;
+	}
+
+	public boolean isbFuncionario() {
+		return bFuncionario;
+	}
+
+	public void setbFuncionario(boolean bFuncionario) {
+		this.bFuncionario = bFuncionario;
+	}
+
 }
